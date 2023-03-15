112be4f966339348af85d7f3b9c58d6287da609b
Brian Faust <hello@basecode.sh>
2023-03-15 16:00:18 +0200

    feat: implement `hackage` integration

--- app/Integrations/Hackage/Provider.php
+++ app/Integrations/Hackage/Provider.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Integrations\Hackage;
 
 use App\Integrations\Contracts\IntegrationProvider;
+use App\Integrations\Hackage\Controllers\LicenseController;
+use App\Integrations\Hackage\Controllers\VersionController;
 use Illuminate\Support\Facades\Route;
 
 final class Provider implements IntegrationProvider
@@ -17,7 +19,8 @@ final class Provider implements IntegrationProvider
     public function register(): void
     {
         Route::prefix('hackage')->group(function (): void {
-            //
+            Route::get('v/{package}', VersionController::class);
+            Route::get('license/{package}', LicenseController::class);
         });
     }
 
