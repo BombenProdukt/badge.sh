e070270950376a2c628d0fd729457fd409fc0a00
Brian Faust <hello@basecode.sh>
2023-03-19 12:23:49 +0200

    refactor: simplify color check for versions

--- app/Actions/DetermineColorByVersion.php
+++ app/Actions/DetermineColorByVersion.php
@@ -4,27 +4,15 @@ declare(strict_types=1);
 
 namespace App\Actions;
 
+use Spatie\Regex\Regex;
+
 final class DetermineColorByVersion
 {
     public static function execute(string $value): string
     {
-        if (str_contains($value, 'alpha')) {
-            return 'cyan.600';
-        }
+        $expression = Regex::match('/alpha|beta|canary|dev|pre|rc|snapshot/', $value);
 
-        if (str_contains($value, 'beta')) {
-            return 'cyan.600';
-        }
-
-        if (str_contains($value, 'canary')) {
-            return 'cyan.600';
-        }
-
-        if (str_contains($value, 'dev')) {
-            return 'cyan.600';
-        }
-
-        if (str_contains($value, 'rc')) {
+        if ($expression->hasMatch()) {
             return 'cyan.600';
         }
 
