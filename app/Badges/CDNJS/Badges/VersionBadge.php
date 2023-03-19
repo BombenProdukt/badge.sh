4aae66d6d1ca04883c425485b6ada0bc7abb3778
Brian Faust <hello@basecode.sh>
2023-03-19 18:17:05 +0200

    feat: implement `cdnjs` service

---
+++ app/Badges/CDNJS/Badges/VersionBadge.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Badges\CDNJS\Badges;
+
+use App\Badges\CDNJS\Client;
+use App\Badges\Templates\VersionTemplate;
+use App\Contracts\Badge;
+use App\Enums\RoutePattern;
+use Illuminate\Routing\Route;
+
+final class VersionBadge implements Badge
+{
+    public function __construct(private readonly Client $client)
+    {
+        //
+    }
+
+    public function handle(string $package): array
+    {
+        return VersionTemplate::make($this->service(), $this->client->get($package));
+    }
+
+    public function service(): string
+    {
+        return 'cdnjs';
+    }
+
+    public function title(): string
+    {
+        return '';
+    }
+
+    public function keywords(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function routePaths(): array
+    {
+        return [
+            '/cdnjs/{package}/version',
+        ];
+    }
+
+    public function routeParameters(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function routeConstraints(Route $route): void
+    {
+        $route->where('package', RoutePattern::CATCH_ALL->value);
+    }
+
+    public function staticPreviews(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function dynamicPreviews(): array
+    {
+        return [
+            '/cdnjs/react/version' => 'version',
+        ];
+    }
+
+    public function deprecated(): array
+    {
+        return [
+            //
+        ];
+    }
+}
