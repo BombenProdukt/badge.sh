a068798c197451ea736c590b37afb168b7c48e10
Brian Faust <hello@basecode.sh>
2023-03-22 07:16:42 +0200

    chore: more grade colors

--- app/Actions/DetermineColorByGrade.php
+++ app/Actions/DetermineColorByGrade.php
@@ -9,14 +9,24 @@ final class DetermineColorByGrade
     public static function execute(string $value): string
     {
         return [
-            'A+' => 'green.600',
-            'A'  => 'green.600',
-            'B'  => 'blue.600',
-            'C'  => 'yellow.600',
-            'D'  => 'amber.600',
-            'E'  => 'orange.600',
-            'F'  => 'red.600',
-            'R'  => 'blue.600',
+            'A+'  => 'green.700',
+            'A'   => 'green.600',
+            'A-'  => 'green.500',
+            'B+'  => 'blue.700',
+            'B'   => 'blue.600',
+            'B-'  => 'blue.500',
+            'C+'  => 'yellow.700',
+            'C'   => 'yellow.600',
+            'C-'  => 'yellow.500',
+            'D+'  => 'amber.700',
+            'D'   => 'amber.600',
+            'D-'  => 'amber.500',
+            'E+'  => 'orange.700',
+            'E'   => 'orange.600',
+            'E-'  => 'orange.500',
+            'F'   => 'red.600',
+            'R'   => 'gray.600',
+            '-'   => 'gray.600',
         ][$value];
     }
 }
