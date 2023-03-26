6e0de54b370266146c9f127e27e697799b74f550
Brian Faust <hello@basecode.sh>
2023-03-26 18:22:55 +0300

    ui: adjust table formatting

--- resources/views/livewire/badge-list.blade.php
+++ resources/views/livewire/badge-list.blade.php
@@ -21,7 +21,7 @@
     </nav>
 
     {{-- Badges --}}
-    <table class="min-w-full divide-y divide-gray-100 mb-16 mt-4">
+    <table class="min-w-full divide-y divide-gray-100 mt-4">
         <thead>
             <tr class="border-gray-100 border-t">
                 <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left font-bold text-gray-900 sm:pl-0">Service</th>
@@ -35,10 +35,10 @@
             @foreach ($previews as $group => $examples)
                 @foreach ($examples as $example)
                     <tr>
-                        <td class="whitespace-nowrap py-2 pl-4 pr-3 text-gray-500 sm:pl-0">
+                        <td class="whitespace-nowrap py-2 pl-4 pr-3 text-gray-500 sm:pl-0 text-sm">
                             {{ $group }}
                         </td>
-                        <td class="whitespace-nowrap px-2 py-2 font-semibold text-gray-900">
+                        <td class="whitespace-nowrap px-2 py-2 font-semibold text-gray-900 text-sm">
                             {{ $example->name }}
                         </td>
                         <td class="whitespace-nowrap px-2 py-2 text-gray-900">
@@ -46,7 +46,7 @@
                                 <x-badge :badge="$example" />
                             </a>
                         </td>
-                        <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right font-medium sm:pr-0">
+                        <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right font-medium sm:pr-0 text-sm">
                             <a href="#" class="text-teal-400 transition font-bold hover:text-teal-900">
                                 Customize
                             </a>
