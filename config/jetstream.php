ec14cd41a48f38c2ca855e10cf9d63c27165048c
Brian Faust <hello@basecode.sh>
2023-03-27 13:16:26 +0300

    chore: disable jetstream features

--- config/jetstream.php
+++ config/jetstream.php
@@ -63,7 +63,7 @@ return [
         // Features::profilePhotos(),
         // Features::api(),
         // Features::teams(['invitations' => true]),
-        Features::accountDeletion(),
+        // Features::accountDeletion(),
     ],
 
     /*
